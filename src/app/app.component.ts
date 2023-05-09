f32ff1f0d4b68ecce1b383337bc56e54c0b114d0
Amirah Parman <fthnamirah@hotmail.com>
2023-05-09 20:00:36 +0200

    User Story 3 completed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'food-order-machine';
+export class AppComponent implements OnInit {
+  restaurantName: string = '';
+
+  ngOnInit(): void {
+    this.restaurantName = 'The Food Shop'
+  }
+
 }
--- src/app/components/landing/landing.component.ts
+++ src/app/components/landing/landing.component.ts
@@ -1,3 +1,5 @@
+import { NgClass } from '@angular/common';
+import { ReturnStatement } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { CapacityService } from 'src/app/services/capacity.service';
 
@@ -11,8 +13,28 @@ export class LandingComponent implements OnInit {
   }
 
   currentCapacity: number = 0;
+  capacityMessage: string = '';
+  orderIsDisabled: boolean = false
 
-  ngOnInit(): void {
+  ngOnInit(): any {
     this.currentCapacity = this.capacityService.getCapacity();
+    this.capacityMessage = this.oneOrZeroSeat();
+    this.orderIsDisabled = this.disabledButton();
+
   }
+
+  oneOrZeroSeat(): string {
+    let currentCapacityInBox = this.currentCapacity;
+    if (currentCapacityInBox === 0)
+      return 'There are no tables available';
+    else if (currentCapacityInBox === 1)
+      return 'there is 1 table available';
+    else
+      return `There are ${this.currentCapacity} tables available`;
+  }
+
+  disabledButton(): boolean {
+    return this.currentCapacity === 0
+  }
+
 }
