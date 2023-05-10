b85e82a73fe07922e1816dad75df5ee4f692b476
Amirah Parman <fthnamirah@hotmail.com>
2023-05-10 15:20:54 +0200

    anchor button to orderpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { AppComponent } from './app.component';
 import { LandingComponent } from './components/landing/landing.component';
 import { HttpClientModule } from '@angular/common/http';
 import { CapacityService } from './services/capacity.service';
+import { OrderPageComponent } from './components/order-page/order-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LandingComponent
+    LandingComponent,
+    OrderPageComponent
   ],
   imports: [
     BrowserModule,
