ea6803efd6404d983e68d50d4692ad4da0129f2c
Amirah Parman <fthnamirah@hotmail.com>
2023-06-07 17:40:02 +0200

    added menu items into the order page

--- src/app/components/order-page/order-page.component.ts
+++ src/app/components/order-page/order-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { MenuGroup } from 'src/app/models/menu-group';
+import { MenuItem } from 'src/app/models/menu-item';
 import { AppSettingsService } from 'src/app/services/app-settings.service';
 import { MenuService } from 'src/app/services/menu.service';
 
@@ -13,17 +14,25 @@ export class OrderPageComponent implements OnInit {
   restaurantName: string = '';
   groupItems: MenuGroup[] = [];
   selectedGroup: MenuGroup = new MenuGroup(0, '', '');
+  menuItemsInGroup: MenuItem[] = [];
 
   ngOnInit(): void {
     this.restaurantName = this.appSettingsService.getRestaurantName();
     this.menuService.getGroups().subscribe((result) => {
       this.groupItems = result;
       this.selectedGroup = this.groupItems[0];
+      this.menuService.getItemsByGroup(this.selectedGroup.id).subscribe((result) => {
+        this.menuItemsInGroup = result;
+      });
     });
   }
 
   groupSelected(thumbnailThatIsClicked: MenuGroup) {
     this.selectedGroup = thumbnailThatIsClicked;
+    
+    this.menuService.getItemsByGroup(this.selectedGroup.id).subscribe((result) => {
+      this.menuItemsInGroup = result;
+    });
   }
 }
 
